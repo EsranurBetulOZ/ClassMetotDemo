57cc8c54cd73b2da3c6c54a6bcd86aa96ed14ac9
esranur <eesranur99@gmail.com>
2021-02-15 16:46:54 +0300

    ödev3

---
+++ MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Eklenen müşteri  ID'si :" + musteri.ID);
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.ID + " ID'li Müşteri silindi");
+        }
+        public void MusteriListe(Musteri musteri)
+        {
+             Console.WriteLine(musteri.ID + " : " + musteri.Adi + " " + musteri.Soyadi);
+             Console.WriteLine("------------------------");
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.ID = 123;
+            musteri1.Adi = "Ahmet";
+            musteri1.Soyadi = "Güney";
+            Musteri musteri2 = new Musteri();
+            musteri2.ID = 1234;
+            musteri2.Adi = "Ayşe";
+            musteri2.Soyadi = "Yılmaz";
+            Musteri musteri3 = new Musteri();
+            musteri3.ID = 1266;
+            musteri3.Adi = "Sevim";
+            musteri3.Soyadi = "Gürsoy";
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+            musteriManager.Delete(musteri3);
+            Console.WriteLine("\n Müşteri listesi \n ");
+            musteriManager.MusteriListe(musteri1);
+            musteriManager.MusteriListe(musteri2);
+            musteriManager.MusteriListe(musteri3);
+
+        }
+    }
+}
